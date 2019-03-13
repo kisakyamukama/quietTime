694e0269b782d3d7232cc2b83ac85d35d3c96b47
Kisakyamukama <kabalimichael03@gmail.com>
2019-03-13 05:27:24 +0300

    add navbar delete unused bootstrap files and link bootstrap on devotion page for the navbar to drop down

--- application/views/devotion/devotions.php
+++ application/views/devotion/devotions.php
@@ -28,6 +28,33 @@
                     $userID =  $this->session->userdata('userID');
                     $user_name =  $this->session->userdata('username');
                  ?>
+                  <!-- navbar -->
+                  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <a class="navbar-brand" href="#">Navbar</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                      <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                        <li class="nav-item active">
+                          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                        </li>
+                      </ul>
+                      <form class="form-inline my-2 my-lg-0">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Search">
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                      </form>
+                    </div>
+                  </nav>
+                  <!-- navbar -->
+
 
         <div class="container">
 
@@ -201,6 +228,6 @@
 
 
 
-
+<script src="<?php echo  base_url('assets/js/bootstrap.min.js'); ?>" charset="utf-8"></script>
 </body>
 </html>
